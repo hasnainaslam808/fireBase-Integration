e029dad9104d5196cee99d0841dc449b821f1ea5
Hasnain Aslam <hasnainaslam808@gmail.com>
2024-01-24 17:26:39 +0500

    CRUD operation

---
+++ src/app/shared/data.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import{AngularFirestore} from '@angular/fire/compat/firestore';
+import { Student } from '../model/student';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private afs:AngularFirestore) { }
+
+
+
+  // add student
+
+  addStudent(student:Student){
+    student.id = this.afs.createId();
+    return this.afs.collection('/students').add(student);
+  }
+
+
+  // get all students
+  getAllStudent(){
+    return this.afs.collection('/students').snapshotChanges();
+  }
+
+  // delete student
+
+  deleteStudent(student:Student){
+  return this.afs.doc('/students/'+student.id).delete();
+  }
+
+  //update student
+
+  updateStudents(student:Student){
+this.deleteStudent(student);
+this.addStudent(student);
+
+  }
+
+  updateStudent(student: Student) {
+    return this.afs.doc('/students/' + student.id).update(student);
+  }
+  
+
+}
